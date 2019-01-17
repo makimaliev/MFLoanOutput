561251725885bf2e1c5071ecbbd34fee61066c4c
mfloan <mfloan@mail.ru>
2019-01-17 15:44:57 +0600

    (===axz===) collateral reports loan data added

--- src/main/java/kg/gov/mf/loan/output/report/model/CollateralArrestFreeView.java
+++ src/main/java/kg/gov/mf/loan/output/report/model/CollateralArrestFreeView.java
@@ -45,6 +45,48 @@ public class CollateralArrestFreeView extends DebtorView
 
 
 
+	@Column
+	private Long v_loan_id;
+
+	@Column
+	private Double v_loan_amount;
+
+	@Column
+	private String v_loan_reg_number;
+
+	@DateTimeFormat(pattern = "dd.mm.yyyy")
+	@Temporal(TemporalType.DATE)
+	private Date v_loan_reg_date;
+
+	@Column
+	private Long v_loan_supervisor_id;
+
+	@Column
+	private  Long v_loan_fin_group_id;
+
+	@Column
+	private Long v_loan_currency_id;
+
+	@Column
+	private Long v_loan_state_id;
+
+	@Column
+	private Long v_loan_type_id;
+
+	@Column
+	private Long v_credit_order_type_id;
+
+	@Column
+	private String v_credit_order_reg_number;
+
+	@DateTimeFormat(pattern = "dd.mm.yyyy")
+	@Temporal(TemporalType.DATE)
+	private Date v_credit_order_reg_date;
+
+
+
+
+
 
 
 	@Column
@@ -295,6 +337,103 @@ public class CollateralArrestFreeView extends DebtorView
 
 	public void setV_ciaf_arrestFreeBy(Long v_ciaf_arrestFreeBy) {
 		this.v_ciaf_arrestFreeBy = v_ciaf_arrestFreeBy;
+	}
+
+
+	public Long getV_loan_id() {
+		return v_loan_id;
+	}
+
+	public void setV_loan_id(Long v_loan_id) {
+		this.v_loan_id = v_loan_id;
+	}
+
+	public Double getV_loan_amount() {
+		return v_loan_amount;
+	}
+
+	public void setV_loan_amount(Double v_loan_amount) {
+		this.v_loan_amount = v_loan_amount;
+	}
+
+	public String getV_loan_reg_number() {
+		return v_loan_reg_number;
+	}
+
+	public void setV_loan_reg_number(String v_loan_reg_number) {
+		this.v_loan_reg_number = v_loan_reg_number;
+	}
+
+	public Date getV_loan_reg_date() {
+		return v_loan_reg_date;
+	}
+
+	public void setV_loan_reg_date(Date v_loan_reg_date) {
+		this.v_loan_reg_date = v_loan_reg_date;
+	}
+
+	public Long getV_loan_supervisor_id() {
+		return v_loan_supervisor_id;
+	}
+
+	public void setV_loan_supervisor_id(Long v_loan_supervisor_id) {
+		this.v_loan_supervisor_id = v_loan_supervisor_id;
+	}
+
+	public Long getV_loan_fin_group_id() {
+		return v_loan_fin_group_id;
+	}
+
+	public void setV_loan_fin_group_id(Long v_loan_fin_group_id) {
+		this.v_loan_fin_group_id = v_loan_fin_group_id;
+	}
+
+	public Long getV_loan_currency_id() {
+		return v_loan_currency_id;
+	}
+
+	public void setV_loan_currency_id(Long v_loan_currency_id) {
+		this.v_loan_currency_id = v_loan_currency_id;
+	}
+
+	public Long getV_loan_state_id() {
+		return v_loan_state_id;
+	}
+
+	public void setV_loan_state_id(Long v_loan_state_id) {
+		this.v_loan_state_id = v_loan_state_id;
+	}
+
+	public Long getV_loan_type_id() {
+		return v_loan_type_id;
+	}
+
+	public void setV_loan_type_id(Long v_loan_type_id) {
+		this.v_loan_type_id = v_loan_type_id;
+	}
+
+	public Long getV_credit_order_type_id() {
+		return v_credit_order_type_id;
+	}
+
+	public void setV_credit_order_type_id(Long v_credit_order_type_id) {
+		this.v_credit_order_type_id = v_credit_order_type_id;
+	}
+
+	public String getV_credit_order_reg_number() {
+		return v_credit_order_reg_number;
+	}
+
+	public void setV_credit_order_reg_number(String v_credit_order_reg_number) {
+		this.v_credit_order_reg_number = v_credit_order_reg_number;
+	}
+
+	public Date getV_credit_order_reg_date() {
+		return v_credit_order_reg_date;
+	}
+
+	public void setV_credit_order_reg_date(Date v_credit_order_reg_date) {
+		this.v_credit_order_reg_date = v_credit_order_reg_date;
 	}
 }
 
