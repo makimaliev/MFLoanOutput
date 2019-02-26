e48c778d046255f2883c12b3660b499000cc22ac
Marlen Akimaliev <makimaliev@github.com>
2019-02-26 13:57:04 +0600

    ( axz ) - calculation fix

--- src/main/java/kg/gov/mf/loan/output/report/utils/CalculationTool.java
+++ src/main/java/kg/gov/mf/loan/output/report/utils/CalculationTool.java
@@ -216,7 +216,7 @@ public class CalculationTool
 //                                if(schedule.getPrincipalPayment()>0) principalPayment+=schedule.getPrincipalPayment();
 //                            }
 
-                            if(childrenLoanSummary.getTotalDisbursed()>0 && loanChildren.getAmount()==0)
+                            if(childrenLoanSummary.getTotalDisbursed()>0 && loanChildren.getAmount()==0 && childrenLoanSummary.getTotalFeePaid()==0)
                             {
                                 childrenLoanSummary.setTotalDisbursed(0.0);
                                 childrenLoanSummary.setTotalOutstanding(childrenLoanSummary.getTotalOutstanding()-childrenLoanSummary.getOutstadingPrincipal());
